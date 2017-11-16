0f80e158238031c7f7cce076a9c14333ebb40f01
anpali <33723569+anpali@users.noreply.github.com>
2017-11-16 21:28:35 +0530

    hello word

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		for(int i=1 ; i<10;i++){
+			System.out.println(" hello class"+i);
+			
+		}
+	}
+
+}
